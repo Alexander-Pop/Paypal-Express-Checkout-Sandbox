d8197759fbcf21c1d9def35969b54b348566fa7f
sasha <alexander.pop.izmail@gmail.com>
2020-02-18 14:28:55 -0600

    Initial from local

---
+++ README.md
@@ -0,0 +1,11 @@
+# Paypal Express Checkout
+
+Checkout page integrated with Paypal Express Checkout.
+
+### Clone and run
+
+```
+php -S localhost:8000
+``` 
+
+![Paypal Express Checkout](images/PayPal-Express-Checkout.png)
---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+$config = array(
+    "environment" => "sandbox",
+    "user"  => "youremal@your.com",
+    "pwd"  => "4354234535",
+    "signature"  => "awewrwerweewwetewtewwteetwetwewewetfooooooooo",
+    "version"  => 113
+);
